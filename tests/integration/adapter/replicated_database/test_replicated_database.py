5a07c8a47b95b98070f804251eb00937f88caaf8
Can Bekleyici <can.bekleyici@deepl.com>
2025-01-08 14:55:01 +0100

    test: add base incremental on replicated db test case

---
+++ tests/integration/adapter/replicated_database/test_replicated_database.py
@@ -0,0 +1,22 @@
+import pytest
+
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.files import model_incremental, schema_base_yml
+
+
+class TestBaseIncrementalReplicatedDatabase(BaseIncremental):
+    @pytest.fixture(scope="class")
+    def test_config(self, test_config):
+        test_config["db_engine"] = "Replicated('/clickhouse/databases/{uuid}', '{shard}', '{replica}')"
+        return test_config
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        config_materialized_incremental = """
+          {{ config(order_by='(some_date, id, name)', inserts_only=True, materialized='incremental', unique_key='id') }}
+        """
+        incremental_sql = config_materialized_incremental + model_incremental
+        return {
+            "incremental.sql": incremental_sql,
+            "schema.yml": schema_base_yml,
+        }
